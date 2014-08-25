8043626a0ef5c8c97836a2383954fc5691699aa2
Johan Borgström <johan@petfactory.se>
2014-08-25 20:57:24 +0200

    added bump depth, remaped spec color/rolloff, add new scene without maps, add refl subdivs

--- convert_mat_maya_to_vray.py
+++ convert_mat_maya_to_vray.py
@@ -9,18 +9,19 @@ def createVrayMat(mat_dict):
         
         # get the maps
         color_file = info_dict.get('color_file')
-        spec_file = info_dict.get('spec_file')
+        spec_color_file = info_dict.get('spec_color_file')
         bump_file = info_dict.get('bump_file')
+        bump_depth = info_dict['bump_depth']
         diffuse_amount_file = info_dict.get('diffuse_amount_file')
         reflect_gloss_file = info_dict.get('reflect_gloss_file')
-        spec_rolloff_color_file = info_dict.get('spec_rolloff_color_file')
+        spec_rolloff_file = info_dict.get('spec_rolloff_file')
         mesh = info_dict.get('mesh')
 
     # create the vray mat
     vrayMat = pm.shadingNode('VRayMtl',asShader=True, name='vray_{0}'.format(mat_name))
     
     vrayMat.useFresnel.set(True)
-
+    vrayMat.reflectionSubdivs.set(64)
     
     if color_file:
         pm.connectAttr('{0}.outColor'.format(color_file), '{0}.color'.format(vrayMat))
@@ -30,16 +31,18 @@ def createVrayMat(mat_dict):
         vrayMat.color.set(color)
 
    
-    if spec_file:
-        pm.connectAttr('{0}.outColor'.format(spec_file), '{0}.reflectionColor'.format(vrayMat))
+    if spec_color_file:
+        pm.connectAttr('{0}.outColor'.format(spec_color_file), '{0}.reflectionColor'.format(vrayMat))
     else:
-        spec = info_dict.get('spec')
-        print('found no spec_file', spec)
-        vrayMat.reflectionColor.set(spec)
+        spec_color = info_dict.get('spec_color')
+        print('found no spec_file', spec_color)
+        vrayMat.reflectionColor.set(spec_color)
              
         
     if bump_file:
         pm.connectAttr('{0}.outColor'.format(bump_file), '{0}.bumpMap'.format(vrayMat))
+        vrayMat.bumpMult.set(bump_depth)
+        
     else:
         print('found no bump_file')
             
@@ -61,12 +64,12 @@ def createVrayMat(mat_dict):
         
         
         
-    if spec_rolloff_color_file:
-        pm.connectAttr('{0}.outColor'.format(spec_rolloff_color_file), '{0}.reflectionColorAmount'.format(vrayMat))
+    if spec_rolloff_file:
+        pm.connectAttr('{0}.outColor'.format(spec_rolloff_file), '{0}.reflectionColorAmount'.format(vrayMat))
     else:
-        spec_rolloff_color = info_dict.get('spec_rolloff_color')
-        print('found no spec_rolloff_color_file', spec_rolloff_color)
-        vrayMat.reflectionColorAmount.set(spec_rolloff_color)
+        spec_rolloff = info_dict.get('spec_rolloff')
+        print('found no spec_rolloff_color_file', spec_rolloff)
+        vrayMat.reflectionColorAmount.set(spec_rolloff)
         #vrayMat.reflectionColorAmount.set(.5)
         
 
@@ -129,8 +132,12 @@ def convert_maya_to_vray_material(mat_list):
         mat_dict = {}
         ret_dict[mat] = mat_dict
         
-        if (mat == 'lambert1' or mat == 'particleCloud1'): continue
+        #if (mat == 'lambert1' or mat == 'particleCloud1'): continue
         
+        # only do blinn materials
+        if type(mat) is not pm.nodetypes.Blinn:
+            continue           
+                
         sg = mat.listConnections(type='shadingEngine')[0]
         mesh = sg.listConnections(type='mesh')
         
@@ -139,29 +146,32 @@ def convert_maya_to_vray_material(mat_list):
         
         
         # get the file textures
-        spec_file = mat.specularRollOff.inputs()
+        spec_color_file = mat.specularColor.inputs()
         
         color_file = mat.color.inputs()
-   
+        
+        bump_file = []
         bump_node = mat.normalCamera.inputs()
+        
         if(bump_node):
             bump_file = bump_node[0].listConnections(type="file")
+            bump_depth = bump_node[0].getAttr('bumpDepth')
          
            
         diffuse_amount_file = mat.diffuse.inputs()
         
         reflect_gloss_file = mat.eccentricity.inputs()
         
-        spec_rolloff_color_file = mat.eccentricity.inputs()
+        spec_rolloff_file = mat.specularRollOff.inputs()
            
         
         # if we did not get a file, check for a value
-        if len(spec_file) > 0:
-            mat_dict['spec_file'] = spec_file[0]
+        if len(spec_color_file) > 0:
+            mat_dict['spec_color_file'] = spec_color_file[0]
             
         else:
-             spec = mat.getSpecularRollOff()
-             mat_dict['spec'] = spec
+             spec_color = mat.getSpecularColor()
+             mat_dict['spec_color'] = spec_color
             
         
         if len(color_file) > 0:
@@ -174,6 +184,8 @@ def convert_maya_to_vray_material(mat_list):
         
         if len(bump_file) > 0:
             mat_dict['bump_file'] = bump_file[0]
+            mat_dict['bump_depth'] = bump_depth
+            
         else:
             pass
         
@@ -192,22 +204,21 @@ def convert_maya_to_vray_material(mat_list):
             reflect_gloss = 1 - eccentricity
             mat_dict['reflect_gloss'] = reflect_gloss
 
-        if len(spec_rolloff_color_file) > 0:
-            mat_dict['spec_rolloff_color_file'] = spec_rolloff_color_file[0]
+        if len(spec_rolloff_file) > 0:
+            mat_dict['spec_rolloff_file'] = spec_rolloff_file[0]
             
         else:
-            spec_rollof = mat.getSpecularColor()
-            #spec_rolloff_color = (spec_rollof[0], spec_rollof[1], spec_rollof[2])
-            #mat_dict['spec_rolloff_color'] = spec_rolloff_color
-            mat_dict['spec_rolloff_color'] = spec_rollof[0]
+            spec_rolloff = mat.getSpecularRollOff()
+            mat_dict['spec_rolloff'] = spec_rolloff
 
     return ret_dict
     
 
 
 pm.openFile('/Users/johan/Developement/maya/convert_mat_maya_to_vray/convert_scene.mb', force=True)
+#pm.openFile('/Users/johan/Developement/maya/convert_mat_maya_to_vray/convert_scene_no_maps.mb', force=True)
 
-all_mat = pm.ls(materials=True)
+all_mat = [mat for mat in pm.ls(materials=True) if type(mat) is pm.nodetypes.Blinn]
 md = convert_maya_to_vray_material(all_mat)
 pprint.pprint(md)
 
